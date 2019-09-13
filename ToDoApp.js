a705a0d97411c3c86d19a15eb4226946fefd35b3
Whitestrife <wolffcole1234@gmail.com>
2019-09-13 00:23:46 -0700

    Working early test
    Only one complete card, can add/delete/check off todo's on the list

---
+++ ToDoApp.js
@@ -0,0 +1,24 @@
+var FadeOut = false;
+
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
+
+$("span:first").on("click", function(event){
+        $("input").fadeToggle();
+});
+
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+$("input[type = 'text']").on("keypress", function(event){
+    if(event.which === 13){
+        var text = $("input[type = 'text']").val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span>" + text + "</li>");
+    }
+});
